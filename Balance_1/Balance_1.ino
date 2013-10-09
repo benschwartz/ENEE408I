1209e4ae18c4e394b93022d639f2cf8ef6f8bf7f
Ben Schwartz <benschwart@gmail.com>
2013-10-09 15:31:58 -0400

    Commented the hell out of everything. Organized the code nicely. Fixed where code runs so that state updates only happen when the IMU interrupt occurs. Calibration routine much more robust. Added Existing Kalman Filter Code.
    Signed-off-by: Ben Schwartz <benschwart@gmail.com>

--- Balance_1/Balance_1.ino
+++ Balance_1/Balance_1.ino
@@ -1,38 +1,44 @@
+//####################################################
+//LIBRARY INCLUDES
+//####################################################
+//Motor Controller
 #include "DualMC33926MotorShield.h"
+//IMU
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
     #include "Wire.h"
 #endif
-
-// Motorshield Object
-
-DualMC33926MotorShield md;
-
-// IMU Object
-
-MPU6050 mpu;
-
-// IMU Output Selection
-//#define OUTPUT_READABLE_QUATERNION
-//#define OUTPUT_READABLE_EULER
-#define OUTPUT_READABLE_YAWPITCHROLL
-//#define OUTPUT_READABLE_REALACCEL
-//#define OUTPUT_READABLE_WORLDACCEL
-//#define OUTPUT_TEAPOT
-
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
-bool blinkState = false;
-
-// MPU control/status vars
+//KALMAN FILTERING
+#include "MatrixMath.h"
+//##################################################
+//CONSTANT DEFINITIONS
+//###################################################
+//CALIBRATION
+#define CAL_ITERS 1000
+#define WAIT_TIME 20000
+//PINS 
+#define CALIB_LED_PIN 11
+//KALMAN FILTERING
+#define N 6
+#define M 3
+#define P 2
+//##################################################
+//GLOBAL VARIABLES
+//###################################################
+//Motor Controller
+DualMC33926MotorShield md; // Motorshield Object
+////////////////////////////////////////////////
+//IMU
+MPU6050 mpu; // IMU Object
+//MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
 uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
 uint8_t fifoBuffer[64]; // FIFO storage buffer
-
-// orientation/motion vars
+//orientation/motion vars
 Quaternion q;           // [w, x, y, z]         quaternion container
 VectorInt16 aa;         // [x, y, z]            accel sensor measurements
 VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
@@ -40,20 +46,69 @@ VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measure
 VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
-double pitch_offset;
-unsigned long start;
-
-
+///////////////////////////////////////////////////
+//KALMAN FILTERING
+float Innovation[N][M] = {
+  {.0774,.0876,0},
+  {0,0,.9967},
+  {.9938,-.0068,0},
+  {0,0,7.9460},
+  {-.0068,.9919,0},
+  {0,0,4.195}
+}; // Innovation gain (Kalman filter)
+float K[P][N] = {
+  {.7071,1.0995,3.1623,2.2409,-6.6562,-1.3915},
+  {.7071,1.0995,-3.1623,-2.2409,-6.6562,-1.3915}
+}; // LQR gain u = -Kx
+float x[N];
+float x_dot[N];
+float u[P];
+float y[M];
+float A[N][N] = {
+  {0,1,0,0,0,0},
+  {0,0,0,0,5.4365,0},
+  {0,0,0,1,0,0},
+  {0,0,0,0,0,0},
+  {0,0,0,0,0,1},
+  {0,0,0,0,61.1608,0}
+};
+float B[N][P] = {
+  {0,0},
+  {-16.6455,-16.6455},
+  {0,0},
+  {146.7373,-146.7373},
+  {0,0},
+  {-57.6542,-57.6542}
+};
+///////////////////////////////////////////////////
+//CALIBRATION
+int calibration_iter;
+double offset_Ax;
+double offset_Ay;
+double offset_Az;
+double offset_yaw;
+double offset_pitch;
+double offset_roll;
+/////////////////////////////////////////////////////
+//TIMING
+unsigned long last_millis;
+/////////////////////////////////////////////////////
 // Control Params
-
 double k = 15;
 double kD = 2;
 double kI = 7;
-
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
-
+/////////////////////////////////////////////////////
+// State Vars
+double yaw;
+double pitch;
+double roll;
+double Ax;
+double Ay;
+double Az;
+double lastPitch;
+double intPitch;
+double dPitch;
+double intAx;
 
 // ================================================================
 // ===               INTERRUPT DETECTION ROUTINE                ===
@@ -64,21 +119,23 @@ void dmpDataReady() {
     mpuInterrupt = true;
 }
 
-
-
 // ================================================================
 // ===                      INITIAL SETUP                       ===
 // ================================================================
 
-
-double lastE = 0;
-double intE = 0;
-int led_pin = 11;
-
 void setup() {
-  pinMode(led_pin, OUTPUT);
-  lastE= 0;
-  intE = 0; // just to make sure they're zeroed
+    Serial.begin(115200);
+    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    //###########################################
+    // PINS
+    //###########################################
+    pinMode(CALIB_LED_PIN, OUTPUT);
+    //###########################################
+    // END PINS
+    //###########################################
+    //###########################################
+    // IMU
+    //###########################################
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
@@ -86,36 +143,27 @@ void setup() {
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
-
-    Serial.begin(115200);
-    while (!Serial); // wait for Leonardo enumeration, others continue immediately
+    //###########################################
+    // END IMU
+    //###########################################
+    //###########################################
+    // MOTOR SHIELD
+    //###########################################
     md.init();
     mpu.initialize();
     devStatus = mpu.dmpInitialize();
-
-    // supply your own gyro offsets here, scaled for min sensitivity
-    mpu.setXGyroOffset(0);
-    mpu.setYGyroOffset(0);
-    mpu.setZGyroOffset(0);
-    mpu.setZAccelOffset(0); // 1688 factory default for my test chip
-
     // make sure it worked (returns 0 if so)
     if (devStatus == 0) {
-        // turn on the DMP, now that it's ready
-     //   Serial.println(F("Enabling DMP..."));
-        mpu.setDMPEnabled(true);
-
-        // enable Arduino interrupt detection
-       // Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
-        attachInterrupt(0, dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
-        // set our DMP Ready flag so the main loop() function knows it's okay to use it
-       // Serial.println(F("DMP ready! Waiting for first interrupt..."));
-        dmpReady = true;
-
-        // get expected DMP packet size for later comparison
-        packetSize = mpu.dmpGetFIFOPacketSize();
+    // turn on the DMP, now that it's ready
+      mpu.setDMPEnabled(true);
+      // enable Arduino interrupt detection
+      // Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+      attachInterrupt(0, dmpDataReady, RISING);
+      mpuIntStatus = mpu.getIntStatus();
+      // set our DMP Ready flag so the main loop() function knows it's okay to use it
+      dmpReady = true;
+      // get expected DMP packet size for later comparison
+      packetSize = mpu.dmpGetFIFOPacketSize();
     } else {
         // ERROR!
         // 1 = initial memory load failed
@@ -125,161 +173,147 @@ void setup() {
         Serial.print(devStatus);
         Serial.println(F(")"));
     }
-    start = millis();
-
-        pinMode(LED_PIN, OUTPUT);
-    digitalWrite(led_pin, HIGH);
-    pitch_offset = ypr[1] * 180/M_PI;
-
-    // configure LED for output
+    //###########################################
+    // END MOTOR SHIELD
+    //###########################################
+    //###########################################
+    // KALMAN FILTER INITIALIZATION
+    //###########################################
+    for(int i=0;i<N;++i) {
+      x[i]=0;
+    }
+    //###########################################
+    // END KALMAN FILTER INITIALIZATION
+    //###########################################
+    //###########################################
+    // CALIBRATION INITIALIZATION
+    //###########################################
+    offset_Ax = 0;
+    offset_Ay = 0;
+    offset_Az = 0;
+    offset_yaw = 0;
+    offset_pitch = 0;
+    offset_roll = 0;
+    calibration_iter = 0;
+    digitalWrite(CALIB_LED_PIN, HIGH);
+    //###########################################
+    // END CALIBRATION INITIALIZATION
+    //###########################################
+    //###########################################
+    // STATE INITIALIZATION
+    //###########################################
+    Ax = 0;
+    Ay = 0;
+    Az = 0;
+    yaw = 0;
+    pitch = 0;
+    roll = 0;
+    intAx = 0;
+    lastPitch = 0;
+    intPitch = 0;
+    dPitch = 0;
+    //###########################################
+    // END STATE INITIALIZATION
+    //###########################################
+    
 }
-
-
 
 // ================================================================
 // ===                    MAIN PROGRAM LOOP                     ===
 // ================================================================
 
 void loop() {
-    // if programming failed, don't try to do anything
-    if (!dmpReady) return;
-
-    // wait for MPU interrupt or extra packet(s) available
-    while (!mpuInterrupt && fifoCount < packetSize) {
-
-
-      // OTHER PROGRAM STUFF?
-
+  if (!dmpReady) return; // if programming failed, don't try to do anything
+  // wait for MPU interrupt or extra packet(s) available
+  while (!mpuInterrupt && fifoCount < packetSize) { 
+    if(calibration_iter > CAL_ITERS){ // check if calibration has finished
+      //######################################################
+      // BEGIN MAIN CONTROL PROGRAM
+      //######################################################        
+      double v_e = k*pitch*abs(pitch) + kD*dPitch + kI*intPitch;
+      md.setM1Speed(v_e);
+      md.setM2Speed(-v_e);
+      //######################################################
+      // END MAIN CONTROL PROGRAM
+      //######################################################    
     }
-
-    // reset interrupt flag and get INT_STATUS byte
-    mpuInterrupt = false;
-    mpuIntStatus = mpu.getIntStatus();
-
-    // get current FIFO count
-    fifoCount = mpu.getFIFOCount();
-
-    // check for overflow (this should never happen unless our code is too inefficient)
-    if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
-        // reset so we can continue cleanly
-        mpu.resetFIFO();
-        Serial.println(F("FIFO overflow!"));
-
+  }
+  //########################################################
+  // BEGIN MPU INTERRUPT ROUTINE
+  //########################################################
+  mpuInterrupt = false; // reset interrupt flag
+  mpuIntStatus = mpu.getIntStatus(); // get INT_STATUS byte
+  fifoCount = mpu.getFIFOCount(); // get current FIFO count
+  // check for overflow (this should never happen unless our code is too inefficient)
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO(); // reset so we can continue cleanly
+    Serial.println(F("FIFO overflow!"));
     // otherwise, check for DMP data ready interrupt (this should happen frequently)
-    } else if (mpuIntStatus & 0x02) {
-        // wait for correct available data length, should be a VERY short wait
-        while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
-
-        // read a packet from FIFO
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        
-        // track FIFO count here in case there is > 1 packet available
-        // (this lets us immediately read more without waiting for an interrupt)
-        fifoCount -= packetSize;
-        
-        mpu.dmpGetQuaternion(&q, fifoBuffer);
-        mpu.dmpGetGravity(&gravity, &q);
-        mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-        double yaw = ypr[0] * 180/M_PI;
-        double pitch = ypr[1] * 180/M_PI;
-        double roll = ypr[2] * 180/M_PI;
-        if(millis()-start<=10000){
-          pitch_offset = ypr[1]*180/M_PI;
-        }
-        else{
-          digitalWrite(LED_PIN,LOW);
-        double e = pitch-pitch_offset;
-        double eDerv = e - lastE;
-        intE += e;
-        double v_e = k*e*abs(e) + kD*eDerv + kI*intE;
-
-            md.setM1Speed(v_e);
-            md.setM2Speed(-v_e);
-        lastE = e;
-        /*#ifdef OUTPUT_READABLE_QUATERNION
-            // display quaternion values in easy matrix form: w x y z
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            Serial.print("quat\t");
-            Serial.print(q.w);
-            Serial.print("\t");
-            Serial.print(q.x);
-            Serial.print("\t");
-            Serial.print(q.y);
-            Serial.print("\t");
-            Serial.println(q.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_EULER
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetEuler(euler, &q);
-            Serial.print("euler\t");
-            Serial.print(euler[0] * 180/M_PI);
-            Serial.print("\t");
-            Serial.print(euler[1] * 180/M_PI);
-            Serial.print("\t");
-            Serial.println(euler[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_YAWPITCHROLL
-            // display Euler angles in degrees
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-            //Serial.print("ypr\t");
-            Serial.print(ypr[0] * 180/M_PI);
-            Serial.print(",");
-            Serial.print(ypr[1] * 180/M_PI);
-            Serial.print(",");
-            Serial.println(ypr[2] * 180/M_PI);
-        #endif
-
-        #ifdef OUTPUT_READABLE_REALACCEL
-            // display real acceleration, adjusted to remove gravity
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            Serial.print("areal\t");
-            Serial.print(aaReal.x);
-            Serial.print("\t");
-            Serial.print(aaReal.y);
-            Serial.print("\t");
-            Serial.println(aaReal.z);
-        #endif
-
-        #ifdef OUTPUT_READABLE_WORLDACCEL
-            // display initial world-frame acceleration, adjusted to remove gravity
-            // and rotated based on known orientation from quaternion
-            mpu.dmpGetQuaternion(&q, fifoBuffer);
-            mpu.dmpGetAccel(&aa, fifoBuffer);
-            mpu.dmpGetGravity(&gravity, &q);
-            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
-            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
-            Serial.print(aaWorld.x);
-            Serial.print(",");
-            Serial.print(aaWorld.y);
-            Serial.print(",");
-            Serial.println(aaWorld.z);
-        #endif
-    
-        #ifdef OUTPUT_TEAPOT
-            // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-        #endif
-        */
-        // blink LED to indicate activity
-        blinkState = !blinkState;
-        digitalWrite(LED_PIN, blinkState);
-        }
+  } else if (mpuIntStatus & 0x02) {
+    // wait for correct available data length, should be a VERY short wait
+  while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize); // read a packet from FIFO
+    // track FIFO count here in case there is > 1 packet available
+    // (this lets us immediately read more without waiting for an interrupt)
+    fifoCount -= packetSize;
+    //######################################################
+    // BEGIN UPDATE MPU STATE
+    //######################################################
+    unsigned long this_millis = millis();
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetAccel(&aa, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+    mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    double yaw_raw = ypr[0] * 180/M_PI;
+    double pitch_raw = ypr[1] * 180/M_PI;
+    double roll_raw = ypr[2] * 180/M_PI;
+    double Ax_raw = aaWorld.x;
+    double Ay_raw = aaWorld.y;
+    double Az_raw = aaWorld.z;
+    // Only begin calibration once the WAIT_TIME has been reached
+    if(this_millis > WAIT_TIME){
+      if(calibration_iter < CAL_ITERS){ // Calibration in progress
+        offset_Ax += Ax_raw;
+        offset_Ay += Ay_raw;
+        offset_Az += Az_raw;
+        offset_yaw += yaw_raw;
+        offset_pitch += pitch_raw;
+        offset_roll += roll_raw;
+        calibration_iter += 1;  
+      }
+      else if(calibration_iter == CAL_ITERS){ // Calibration finished
+        offset_Ax /= CAL_ITERS;
+        offset_Ay /= CAL_ITERS;
+        offset_Az /= CAL_ITERS;
+        offset_yaw /= CAL_ITERS;
+        offset_pitch /= CAL_ITERS;
+        offset_roll /= CAL_ITERS;
+        calibration_iter += 1; // never do this again
+        digitalWrite(CALIB_LED_PIN,LOW);
+      }
+      else {  // Post calibration - update state to account for offsets and integrate x
+        Ax = Ax_raw-offset_Ax;
+        Ay = Ay_raw-offset_Ay;
+        Az = Az_raw-offset_Az;
+        yaw = yaw_raw-offset_yaw;
+        pitch = pitch_raw-offset_pitch;
+        roll = roll_raw-offset_roll;
+        double delta_t = (this_millis-last_millis)*.001;
+        dPitch = pitch - lastPitch;
+        intPitch += dPitch*delta_t;
+        intAx += Ax*delta_t;
+        lastPitch = pitch;
+      }
+      last_millis=this_millis;
     }
+    //######################################################
+    // END UPDATE MPU STATE
+    //######################################################
+  }
+  //########################################################
+  // END MPU INTERRUPT ROUTINE
+  //########################################################  
 }
